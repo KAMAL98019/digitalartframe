77fcb750450875d0bb7390b2a2cc664e14b194a8
Kamalesh B <kamalesh944555@gmail.com>
2026-04-05 15:49:16 +0530

    main.ts fixed

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Server running on port ${port}`);
 }
 void bootstrap();
