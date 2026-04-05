01ca47b17539bdf29c53f2dc156000e85572dcee
Kamalesh B <kamalesh944555@gmail.com>
2026-04-05 15:55:25 +0530

    ipv4 fixed

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
